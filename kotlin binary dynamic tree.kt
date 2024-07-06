a6384488089a163715d25a134bc94b8a0dbd6422
Vohn Daxen Sobresida <sobresidavohndaxen@gmail.com>
2024-07-06 23:50:06 +0800

    w pointers

--- kotlin binary dynamic tree.kt
+++ kotlin binary dynamic tree.kt
@@ -37,3 +37,60 @@ fun main() {
     inorderTraversal(root)
     println()
 }
+
+
+*/
+
+
+class TreeNode(var data: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+fun insert(root: TreeNode?, value: Int): TreeNode {
+    return if (root == null) {
+        TreeNode(value)
+    } else if (value < root.data) {
+        root.left = insert(root.left, value)
+        root
+    } else if (value > root.data) {
+        root.right = insert(root.right, value)
+        root
+    } else {
+        root
+    }
+}
+
+fun inorderTraversal(root: TreeNode?) {
+    if (root != null) {
+        inorderTraversal(root.left)
+        print("${root.data} ")
+        inorderTraversal(root.right)
+    }
+}
+
+fun main() {
+    var root: TreeNode? = null
+    var nodeCount = 0
+    val scanner = java.util.Scanner(System.`in`)
+    var choice: Int
+
+    do {
+        print("Do you want to input a node? [1 for yes, 0 for no]: ")
+        choice = scanner.nextInt()
+
+        if (choice == 1) {
+            print("Enter node ${++nodeCount}: ")
+            val value = scanner.nextInt()
+            root = insert(root, value)
+        }
+    } while (choice != 0)
+
+    println("Alright, you're done inputting.")
+    print("Now, the inorder traversal of the tree is: ")
+    inorderTraversal(root)
+    println()
+
+
+
+*/
